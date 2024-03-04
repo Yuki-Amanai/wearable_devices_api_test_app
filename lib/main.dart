8dfbdcf16f4f196289016a75ec1d610eb1ee483b
Yuki-Amanai <y.amanai@zenplace.co.jp>
2024-03-04 11:25:46 +0900

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:health/health.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
+void main() {
+  runApp(
+    ProviderScope(child: MyApp()),
+  );
 }
 
-class _MyAppState extends State<MyApp> {
+class HealthDataNotifier extends StateNotifier<List<HealthDataPoint>> {
   HealthFactory health = HealthFactory();
-  List<HealthDataPoint> _healthDataList = [];
-  bool _isFetching = false;
 
-  @override
-  void initState() {
-    super.initState();
+  HealthDataNotifier() : super([]) {
     fetchData();
   }
 
   void fetchData() async {
     DateTime startDate = DateTime.now();
     DateTime endDate = DateTime.now();
-
-    setState(() {
-      _isFetching = true;
-    });
-
-    // 歩数データのタイプを指定
     List<HealthDataType> types = [
       HealthDataType.STEPS,
       HealthDataType.HEART_RATE,
       HealthDataType.RESTING_HEART_RATE,
-      HealthDataType.DISTANCE_WALKING_RUNNING,
     ];
-    List<HealthDataPoint> healthData = [];
 
-    // データの取得
     try {
       bool accessGranted = await health.requestAuthorization(types);
       if (accessGranted) {
-        healthData = await health.getHealthDataFromTypes(startDate, endDate, types);
+        List<HealthDataPoint> healthData = await health.getHealthDataFromTypes(startDate, endDate, types);
+        state = HealthFactory.removeDuplicates(healthData);
       }
     } catch (e) {
       print("Error fetching health data: $e");
     }
 
-    // 取得したデータをフィルタリングしてリストに追加
-    _healthDataList = HealthFactory.removeDuplicates(healthData);
-    print(_healthDataList);
-
-    setState(() {
-      _isFetching = false;
-    });
-
-    // ここで次のポーリングをスケジュールします（例：60秒後）
-    Future.delayed(const Duration(seconds: 60)).then((_) {
-      if (mounted) {
-        fetchData();
-      }
-    });
+    // 次のポーリングをスケジュール
+    Future.delayed(const Duration(seconds: 60), fetchData);
   }
+}
 
+final healthDataProvider = StateNotifierProvider.autoDispose<HealthDataNotifier, List<HealthDataPoint>>((ref) => HealthDataNotifier());
+
+class MyApp extends HookConsumerWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final healthData = ref.watch(healthDataProvider);
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Health Data Fetch Example'),
+          title: const Text('Health Data Fetch Example'),
         ),
         body: Center(
-          child: _isFetching
+          child: healthData.isEmpty
               ? const CircularProgressIndicator()
-              : Text('Steps Today: ${_healthDataList.fold<int>(0, (sum, item) => sum + int.parse(item.value.toString()))}'),
+              : Text('Steps Today: ${healthData.fold<int>(0, (sum, item) => sum + int.parse(item.value.toString()))}'),
         ),
       ),
     );
   }
 }
-
