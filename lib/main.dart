1f7b410eb4ed7e1d8af7a7f7d410417d35e53768
Yuki-Amanai <y.amanai@zenplace.co.jp>
2024-03-06 22:15:39 +0900

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:health/health.dart';
+import 'dart:io';
+import 'package:wearable_devices_api_test_app/view_model/health_view_model.dart';
 
 void main() {
   runApp(
@@ -8,43 +9,11 @@ void main() {
   );
 }
 
-class HealthDataNotifier extends StateNotifier<AsyncValue<List<HealthDataPoint>>> {
-  final health = HealthFactory();
-
-  HealthDataNotifier() : super(const AsyncLoading()) {
-    fetchData();
-  }
-
-  void fetchData() async {
-    DateTime startDate = DateTime.now().subtract(const Duration(days: 1));
-    DateTime endDate = DateTime.now();
-    List<HealthDataType> types = [
-      HealthDataType.STEPS,
-      HealthDataType.HEART_RATE,
-      HealthDataType.RESTING_HEART_RATE,
-    ];
-
-    try {
-      bool accessGranted = await health.requestAuthorization(types);
-      if (accessGranted) {
-        List<HealthDataPoint> healthData = await health.getHealthDataFromTypes(startDate, endDate, types);
-        state = AsyncData(HealthFactory.removeDuplicates(healthData));
-      }
-    } catch (e) {
-      print(("Error fetching health data: $e"));
-    }
-
-    // 次のポーリングをスケジュール
-    Future.delayed(const Duration(seconds: 20), fetchData);
-  }
-}
-
-final healthDataProvider = StateNotifierProvider.autoDispose<HealthDataNotifier, AsyncValue<List<HealthDataPoint>>>((ref) => HealthDataNotifier());
-
 class MyApp extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final healthData = ref.watch(healthDataProvider);
+    final isSupported = !Platform.isAndroid;
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -53,7 +22,8 @@ class MyApp extends HookConsumerWidget {
         body: Center(
           child : healthData.when(data: (data) {
             final healthList = healthData.valueOrNull ?? [];
-            return  ListView.builder(
+            return isSupported ?
+              ListView.builder(
                 physics:const ScrollPhysics(),
                 itemCount: healthList.length,
                 itemBuilder: (_, index) {
@@ -63,7 +33,8 @@ class MyApp extends HookConsumerWidget {
                     trailing: Text('${health.unitString} '),
                     subtitle: Text('${health.dateFrom} - ${health.dateTo}'),
                   );
-                });
+                })
+                : const Text('現在Androidはサポート外です');
           }, error:(e, stacktrace) => Text(e.toString()),
               loading: () => const Center(
                 child:  CircularProgressIndicator(),
--- lib/view_model/health_view_model.dart
+++ lib/view_model/health_view_model.dart
@@ -1,58 +1,38 @@
 import 'package:flutter/cupertino.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:health/health.dart';
-import 'package:wearable_devices_api_test_app/main.dart';
+import 'dart:io';
 
-enum HealthState {
-  DATA_NOT_FETCHED,
-  FETCHING_DATA,
-  DATA_READY,
-  NO_DATA,
-  AUTH_NOT_GRANTED,
-  INIT,
-  LOADING,
-  ERROR_AUTHORIZE,
-  ERROR_NO_DATA,
-  SUCCESS,
-}
+class HealthDataNotifier extends StateNotifier<AsyncValue<List<HealthDataPoint>>> {
+  final health = HealthFactory();
 
- List<HealthDataType> healthDataTypes = [
-  HealthDataType.STEPS,
-   HealthDataType.HEART_RATE,
-   HealthDataType.RESTING_HEART_RATE,
-   HealthDataType.DISTANCE_WALKING_RUNNING,
-];
-
-final healthDataAccessPermission = [
-  HealthDataAccess.READ_WRITE,
-  HealthDataAccess.READ_WRITE,
-];
-
-final healthProvider = StateNotifierProvider.autoDispose<HealthNotifier, AsyncValue<List<HealthDataPoint>>>((ref) => HealthNotifier(ref));
-
-class HealthNotifier extends StateNotifier<AsyncValue<List<HealthDataPoint>>> {
-  HealthNotifier(this.ref) : super(const AsyncLoading()) {
-    fetchHealthData();
+  HealthDataNotifier() : super(const AsyncLoading()) {
+    fetchData();
   }
 
-  Ref ref;
+  void fetchData() async {
+    DateTime startDate = DateTime.now().subtract(const Duration(days: 1));
+    DateTime endDate = DateTime.now();
+    List<HealthDataType> types = [
+      HealthDataType.STEPS,
+      HealthDataType.HEART_RATE,
+      HealthDataType.RESTING_HEART_RATE,
+    ];
 
-  final health = HealthFactory(useHealthConnectIfAvailable: true);
-
-  Future<void> fetchHealthData() async {
     try {
-      DateTime startDate = DateTime.now();
-      DateTime endDate = DateTime.now();
-      bool accessWasGranted = await health.requestAuthorization(healthDataTypes);
-      if (!accessWasGranted) {
-        print('エラー');
-        return;
+      bool accessGranted = await health.requestAuthorization(types);
+      if (accessGranted && isSupported) {
+        List<HealthDataPoint> healthData = await health.getHealthDataFromTypes(startDate, endDate, types);
+        state = AsyncData(HealthFactory.removeDuplicates(healthData));
+      } else {
       }
-      state = const AsyncLoading();
-      final healthData = await health.getHealthDataFromTypes(startDate, endDate, healthDataTypes);
-      state = AsyncData(healthData);
     } catch (e) {
-      throw e.toString();
+      print(("Error fetching health data: $e"));
     }
+
+    // 次のポーリングをスケジュール
+    Future.delayed(const Duration(seconds: 20), fetchData);
   }
 }
+
+final healthDataProvider = StateNotifierProvider.autoDispose<HealthDataNotifier, AsyncValue<List<HealthDataPoint>>>((ref) => HealthDataNotifier());
